62de2c7aa90138dc1c5d820416e5d0637d3ffddf
SpSf2 <kiworks.duglas@gmail.com>
2026-07-13 11:00:47 +0200

    TagController implementado

--- src/main/java/com/example/controller/TagController.java
+++ src/main/java/com/example/controller/TagController.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -72,4 +76,59 @@ public class TagController {
     }
 
 
+    //Añadir una Etiqueta existente a un Tutorial en particular
+    @PostMapping("/tutorials/{tutorialId}/tags")
+    public ResponseEntity<Tag> addTag(@PathVariable Long tutorialId, @RequestBody Tag tagRequest) {
+        Tag tag = tutorialRepository.findById(tutorialId).map(tutorial -> {
+            long tagId = tagRequest.getId();
+
+            // tag is existed
+            if (tagId != 0L) {
+                Tag _tag = tagRepository.findById(tagId)
+                        .orElseThrow(() -> new ResourceNotFoundException("Not found Tag with id = " + tagId));
+                tutorial.addTag(_tag);
+                tutorialRepository.save(tutorial);
+                return _tag;
+            }
+
+            // add and create new Tag
+            tutorial.addTag(tagRequest);
+            return tagRepository.save(tagRequest);
+        }).orElseThrow(() -> new ResourceNotFoundException("Not found Tutorial with id = " + tutorialId));
+
+        return new ResponseEntity<>(tag, HttpStatus.CREATED);
+    }
+
+
+    @PutMapping("/tags/{id}")
+    public ResponseEntity<Tag> updateTag(@PathVariable long id, @RequestBody Tag tagRequest) {
+        Tag tag = tagRepository.findById(id)
+        .orElseThrow(() -> new ResourceNotFoundException("TagId " + id + "not found"));
+
+        tag.setName(tagRequest.getName());
+
+        return new ResponseEntity<>(tagRepository.save(tag), HttpStatus.OK);
+    }
+
+    //Eliminar una Etiqueta de un Tutorial en particular
+    @DeleteMapping("/tutorials/{tutorialId}/tags/{tagId}")
+    public ResponseEntity<HttpStatus> deleteTagFromTutorial(@PathVariable Long tutorialId, 
+                                                            @PathVariable Long tagId) {
+        Tutorial tutorial = tutorialRepository.findById(tutorialId)
+            .orElseThrow(() -> new ResourceNotFoundException("Not found Tutorial with id = " + tutorialId));
+
+        tutorial.removeTag(tagId);
+        tutorialRepository.save(tutorial);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
+    @DeleteMapping("/tags/{id}")
+    public ResponseEntity<HttpStatus> deleteTag(@PathVariable long id) {
+       
+        tagRepository.deleteById(id);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
